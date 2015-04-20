665f5ddc5990f251bedfdd51e077641748ccd8a9
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-04-20 12:19:18 -0600

    set up first test

--- test/spec/TimeseriesModelTest.js
+++ test/spec/TimeseriesModelTest.js
@@ -4,3 +4,11 @@
 var expect = chai.expect,
     TimeseriesModel = require('TimeseriesModel'),
     Xhr = require('util/Xhr');
+
+describe('TimeseriesModel Test', function () {
+  describe('constructor', function () {
+    it('is defined', function () {
+      expect(TimeseriesModel).to.not.equal(null);
+    });
+  });
+});
