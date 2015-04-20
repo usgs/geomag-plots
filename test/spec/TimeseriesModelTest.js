ad0c2efe7703c15941552890f43bea837b967f2b
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-04-20 12:40:28 -0600

    added xhr call and test for data

--- test/spec/TimeseriesModelTest.js
+++ test/spec/TimeseriesModelTest.js
@@ -2,12 +2,31 @@
 'use strict';
 
 var expect = chai.expect,
-    TimeseriesModel = require('TimeseriesModel');
+    TimeseriesModel = require('TimeseriesModel'),
+    Xhr = require('util/Xhr');
 
 describe('TimeseriesModel Test', function () {
   describe('constructor', function () {
     it('is defined', function () {
       expect(TimeseriesModel).to.not.equal(null);
+    });
+  });
+
+  describe('Test getGaps', function () {
+    var response;
+
+    before(function (done) {
+      Xhr.ajax({
+        url: 'observatory_data.json',
+        success: function (data) {
+          response = TimeseriesResponse(data);
+          done();
+        }
+      });
+    }
+
+    it('returns gaps in data', function () {
+      expect(response.getGaps().to.equal(50));
     });
   });
 });
