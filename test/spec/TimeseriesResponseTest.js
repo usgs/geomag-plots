bb9e2f11ec06be62c26f2e6ab2c8704849b53001
Eddie McWhirter <emcwhirter@usgs.gov>
2015-04-20 11:56:59 -0600

    Start file for TimeseriesResponse tests.

---
+++ test/spec/TimeseriesResponseTest.js
@@ -0,0 +1,37 @@
+/* global before, chai, describe, it */
+'use strict';
+
+var expect = chai.expect,
+    TimeseriesResponse = require('TimeseriesResponse'),
+    Xhr = require('util/Xhr');
+
+
+describe('TimeseriesResponse Test', function () {
+
+  describe('constructor', function () {
+    it('is defined', function () {
+      expect(TimeseriesResponse).to.not.equal(null);
+    });
+  });
+
+  describe('getTimeseries', function () {
+    var response;
+
+    before(
+      function (done) {
+        Xhr.ajax({
+          url: 'observatory_data.json',
+          success: function (data) {
+            response = TimeseriesResponse(data);
+            done();
+          }
+        });
+      }
+    );
+
+    it('Return correct number of Timeseries objects', function () {
+      expect(response.getTimeseries().length).to.equal(1);
+    });
+  });
+
+});
