9c9d53c1f4e02eeb77c6d21f1975e3dbfd44571f
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-04-20 15:02:35 -0600

    added tests for gap data and times

--- test/spec/TimeseriesModelTest.js
+++ test/spec/TimeseriesModelTest.js
@@ -1,4 +1,4 @@
-/* global chai, describe, it */
+/* global before, chai, describe, it */
 'use strict';
 
 var expect = chai.expect,
@@ -19,14 +19,29 @@ describe('TimeseriesModel Test', function () {
       Xhr.ajax({
         url: 'observatory_data.json',
         success: function (data) {
-          response = TimeseriesResponse(data);
+          response = TimeseriesModel({
+            times: data.times,
+            values: data.data[4].values.H
+          });
           done();
         }
       });
-    }
+    });
 
     it('returns gaps in data', function () {
-      expect(response.getGaps().to.equal(50));
+      expect(response.getGaps().length).to.equal(2);
+    });
+
+    it('returns start time for gap data', function () {
+      var gap = response.getGaps()[0];
+
+      expect(gap.start).to.equal(1429480440);
+    });
+
+    it('returns end time for gap data', function () {
+      var gap = response.getGaps()[0];
+
+      expect(gap.end).to.equal(1429481280);
     });
   });
 });
