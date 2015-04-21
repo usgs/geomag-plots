e300937f90bceaa5e511660273a2e69d9236fdb8
Eddie McWhirter <emcwhirter@usgs.gov>
2015-04-21 10:10:18 -0600

    Do some stuff.

--- example/TimeseriesView.js
+++ example/TimeseriesView.js
@@ -1,29 +1,11 @@
 'use strict';
 
 
-var TimeseriesView = require('TimeseriesView'),
-    Xhr = require('util/Xhr');
+var TimeseriesView = require('TimeseriesView');
 
-var el = document.querySelector('#example');
+var el = document.querySelector('#example'),
+    meta = document.createElement('div'),
+    trace = document.createElement('div');
 
-
-Xhr.ajax({
-  url: 'observatory_data.json',
-  success: function (data) {
-    var times;
-
-    times = data.times.map(function (t) {
-      return new Date(t*1000);
-    });
-
-    data.data.forEach(function (obs) {
-      var value;
-
-      for (value in obs.values) {
-        TimeseriesView({
-          el: el.appendChild(document.createElement('div'))
-        }).render();
-      }
-    });
-  }
-});
+el.appendChild(meta);
+el.appendChild(trace);
--- src/htdocs/js/TimeseriesView.js
+++ src/htdocs/js/TimeseriesView.js
@@ -14,15 +14,29 @@ var TimeseriesView = function (options) {
   var _this,
       _initialize,
 
-      _el;
+      _el,
+      _meta,
+      _trace;
 
   _this = View(options);
 
-  _initialize = function () {
+  _initialize = function (options) {
     _el = options._el;
 
-  };
+    var series = TimeseriesResponse();
+    console.log(series);
 
+    _el.innerHTML = [
+      '<div class="meta-data"></div>',
+      '<div class="trace-data"></div>'
+    ].join('');
+
+    _meta = _el.querySelector('meta-data');
+    _trace = _el.querySelector('trace-data');
+
+    _meta.innerHTML = 'Meta';
+    _trace.innerHTML = 'Trace';
+  };
 
   _initialize(options);
   options = null;
