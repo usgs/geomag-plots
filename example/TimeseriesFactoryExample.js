748a3fa8692dcfe4d7b2e27966516fadcef15d43
Hal Simpson <hasimpson@usgs.gov>
2015-04-20 18:11:55 -0600

    Added some print for the example, also added console.log, so people can look at the full json if they want to

--- example/TimeseriesFactoryExample.js
+++ example/TimeseriesFactoryExample.js
@@ -9,6 +9,11 @@ timeseriesFactory.getTimeseries({
   observatory:'BOU',
   channel: null,
   starttime: new Date(1429459810000),
-  endtime: new Date(1429546200000)
-  });
+  endtime: new Date(1429546200000),
+  callback: function (response) {
+    var el = document.getElementById('example');
+    el.innerHTML = response.data[0].id;
+    console.log(response);
+   }
+});
 
