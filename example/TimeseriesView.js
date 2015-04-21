ddadbca8f9d61e0bd94aa5687d0fe8efb231e303
Eddie McWhirter <emcwhirter@usgs.gov>
2015-04-21 11:10:25 -0600

    Grab 1 timeseries from TimeseriesResponse to display.

--- example/TimeseriesView.js
+++ example/TimeseriesView.js
@@ -1,11 +1,30 @@
 'use strict';
 
+var d3 = require('d3'),
+    D3TimeseriesView = require('D3TimeseriesView'),
+    TimeseriesResponse = require('TimeseriesResponse'),
+    // TimeseriesView = require('TimeseriesView'),
+    Xhr = require('util/Xhr');
 
-var TimeseriesView = require('TimeseriesView');
+var el = document.querySelector('#example');
 
-var el = document.querySelector('#example'),
-    meta = document.createElement('div'),
-    trace = document.createElement('div');
-
-el.appendChild(meta);
-el.appendChild(trace);
+Xhr.ajax({
+  url: 'observatory_data.json',
+  success: function (data) {
+    var timeseries = TimeseriesResponse(data).getTimeseries()[0];
+    // TimeseriesView({
+    //   timeseries: timeseries
+    // });
+    var meta = timeseries.get('metadata');
+    D3TimeseriesView({
+      el: el.appendChild(document.createElement('div')),
+      data: timeseries,
+      height: 300,
+      width: 960,
+      xAxisLabel: 'Time (UTC)',
+      xAxisScale: d3.time.scale.utc(),
+      yAxisLabel: meta.observatory + ' ' + meta.channel + ' (nT)',
+      yAxisScale: d3.scale.linear()
+    }).render();
+  }
+});
