8ed598fc33101e863067ae6ad176ac686be88d77
Eddie McWhirter <emcwhirter@usgs.gov>
2015-04-21 10:40:25 -0600

    Add destroy and render.

--- src/htdocs/js/TimeseriesView.js
+++ src/htdocs/js/TimeseriesView.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var TimeseriesResponse = require('TimeseriesResponse'),
-    View = require('mvc/View');
+var Util = require('util/Util'),
+    View = require('mvc/View'),
+
+    D3TimeseriesView = require('D3TimeseriesView');
 
 
 /**
@@ -14,28 +16,46 @@ var TimeseriesView = function (options) {
   var _this,
       _initialize,
 
-      _el,
-      _meta,
-      _trace;
+      _timeseries;
 
   _this = View(options);
 
   _initialize = function (options) {
-    _el = options._el;
+    _this.el.innerHTML = '<p>Timeseries View is here.</p>';
 
-    var series = TimeseriesResponse();
-    console.log(series);
+    _timeseries = options.timeseries;
+    // var series = TimeseriesResponse();
+    // console.log(series);
 
-    _el.innerHTML = [
-      '<div class="meta-data"></div>',
-      '<div class="trace-data"></div>'
-    ].join('');
+    // _el.innerHTML = [
+    //   '<div class="meta-data"></div>',
+    //   '<div class="trace-data"></div>'
+    // ].join('');
 
-    _meta = _el.querySelector('meta-data');
-    _trace = _el.querySelector('trace-data');
+    // _meta = _el.querySelector('meta-data');
+    // _trace = _el.querySelector('trace-data');
 
-    _meta.innerHTML = 'Meta';
-    _trace.innerHTML = 'Trace';
+    // _meta.innerHTML = 'Meta';
+    // _trace.innerHTML = 'Trace';
+
+    _this.render();
+  };
+
+  /**
+   * Destroy this view.
+   */
+  _this.destroy = Util.compose(function () {
+    _this = null;
+  }, _this.destroy);
+
+  _this.render = function () {
+    var meta = _timeseries.get('metadata');
+    D3TimeseriesView({
+      el: _this.el.appendChild(document.createElement('div')),
+      data: _timeseries,
+      xAxisLabel: 'Time (UTC)',
+      yAxisLabel: meta.observatory + ' ' + meta.channel + ' (nT)'
+    }).render();
   };
 
   _initialize(options);
