a3aaeeb6cc9c5faa97e0ba950450497b500d94c9
Eddie McWhirter <emcwhirter@usgs.gov>
2015-04-21 13:29:46 -0600

    Add el to TimeseriesView object, remove commented code.

--- example/TimeseriesView.js
+++ example/TimeseriesView.js
@@ -3,7 +3,7 @@
 var d3 = require('d3'),
     D3TimeseriesView = require('D3TimeseriesView'),
     TimeseriesResponse = require('TimeseriesResponse'),
-    // TimeseriesView = require('TimeseriesView'),
+    TimeseriesView = require('TimeseriesView'),
     Xhr = require('util/Xhr');
 
 var el = document.querySelector('#example');
@@ -11,20 +11,9 @@ var el = document.querySelector('#example');
 Xhr.ajax({
   url: 'observatory_data.json',
   success: function (data) {
-    var timeseries = TimeseriesResponse(data).getTimeseries()[0];
-    // TimeseriesView({
-    //   timeseries: timeseries
-    // });
-    var meta = timeseries.get('metadata');
-    D3TimeseriesView({
-      el: el.appendChild(document.createElement('div')),
-      data: timeseries,
-      height: 300,
-      width: 960,
-      xAxisLabel: 'Time (UTC)',
-      xAxisScale: d3.time.scale.utc(),
-      yAxisLabel: meta.observatory + ' ' + meta.channel + ' (nT)',
-      yAxisScale: d3.scale.linear()
-    }).render();
+    TimeseriesView({
+      el: el,
+      timeseries: TimeseriesResponse(data).getTimeseries()[0]
+    });
   }
 });
