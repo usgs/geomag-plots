62d34ba20750cc38e044bed2891de3217de6ffa7
Hal Simpson <hasimpson@usgs.gov>
2015-04-22 14:07:22 -0600

    added test to check view is updating off collection

--- test/spec/TimeseriesCollectionViewTest.js
+++ test/spec/TimeseriesCollectionViewTest.js
@@ -1,7 +1,11 @@
-/* global describe, it */
+/* global chai, describe, it */
 'use strict';
 
-var TimeseriesCollectionView = require('TimeseriesCollectionView');
+var expect = chai.expect,
+
+    Collection = require('mvc/Collection'),
+    Timeseries = require('Timeseries'),
+    TimeseriesCollectionView = require('TimeseriesCollectionView');
 
 describe('TimeseriesCollectionView test suite.', function () {
   describe('Constructor', function () {
@@ -9,5 +13,26 @@ describe('TimeseriesCollectionView test suite.', function () {
       var view = TimeseriesCollectionView();
       view.destroy();
     });
+  });
+
+  describe('view collection is updated.', function () {
+    var collection = Collection(),
+        timeseriesCollectionView = TimeseriesCollectionView({
+            el:document.createElement('div'),
+            collection:collection}),
+        timeseries = Timeseries({
+          id: 0,
+          times: [1],
+          values: [1],
+          metadata: {
+            observatory: 'BOU',
+            channel: 'H',
+            nominal: 0
+          }
+        });
+
+    collection.add(timeseries);
+    expect(timeseriesCollectionView.el.childNodes[0].
+        childNodes.length).to.equal(1);
   });
 });
