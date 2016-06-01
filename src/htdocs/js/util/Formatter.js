5baa1d616be862c4439058c4f0c6c5ee8b3d3bd3
Eric Martinez <emartinez@usgs.gov>
2016-06-01 12:09:17 -0600

    Added formatter class for general formatting.

---
+++ src/htdocs/js/util/Formatter.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var _iso8601;
+
+
+_iso8601 = function (date) {
+  var day,
+      hour,
+      minute,
+      month,
+      second,
+      year;
+
+  year = date.getUTCFullYear();
+  month = date.getUTCMonth();
+  day = date.getUTCDate();
+
+  hour = date.getUTCHours();
+  minute = date.getUTCMinutes();
+  second = date.getUTCSeconds();
+
+  if (month < 10) {
+    month = '0' + month;
+  }
+
+  if (day < 10) {
+    day = '0' + day;
+  }
+
+  if (hour < 10) {
+    hour = '0' + hour;
+  }
+
+  if (minute < 10) {
+    minute = '0' + minute;
+  }
+
+  if (second < 10) {
+    second = '0' + second;
+  }
+
+  return year + '-' + month + '-' + day + 'T' +
+      hour + ':' + minute + ':' + second + 'Z';
+};
+
+
+module.exports = {
+  iso8601: _iso8601
+};
