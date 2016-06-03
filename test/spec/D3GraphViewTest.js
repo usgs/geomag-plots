d128b6a0f4e6c7f23011765481a552e9db9e6493
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-06-03 12:43:32 -0600

    added test for renderZoom which is the new method that was created

--- test/spec/D3GraphViewTest.js
+++ test/spec/D3GraphViewTest.js
@@ -1,22 +1,53 @@
-/* global chai, describe, it */
+/* global chai, describe, it, sinon */
 'use strict';
 
-var D3GraphView = require('plots/D3GraphView');
+var D3GraphView = require('plots/D3GraphView'),
+    Model = require('mvc/Model');
 
 var expect = chai.expect;
+
+var changes,
+    plotModel;
+
+changes = {
+  'zoomScale': 2,
+  'zoomTranslate': [
+    1,
+    2
+  ]
+
+};
+
+plotModel = Model({
+  'zoomScale': 1,
+  'zoomTranslate': [
+    -0.8617363393199184,
+    -0.11143880419554364
+  ]
+});
 
 describe('D3GraphViewTest', function () {
   describe('constructor', function () {
     it('is defined', function () {
       expect(typeof D3GraphView).to.equal('function');
     });
+  });
 
-    // it('can be destroyed', function () {
-    //   var view;
-    //
-    //   view = D3GraphView();
-    //
-    //   expect(view.destroy).to.not.throw(Error);
-    // });
+  describe('renderZoom', function () {
+    it('calls render', function () {
+      var view;
+
+      view = D3GraphView({
+        plotModel: plotModel
+      });
+
+      sinon.stub(view, 'render', function () {});
+      view.renderZoom(changes);
+
+      expect(view.render.callCount).to.equal(1);
+
+      view.render.restore();
+      view.destroy();
+    });
   });
 });
