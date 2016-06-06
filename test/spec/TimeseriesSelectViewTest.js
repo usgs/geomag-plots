31f735c68b10f9ea762ae9aee9f987f42bda6456
Eric Martinez <emartinez@usgs.gov>
2016-06-06 14:16:31 -0600

    Integrated CompactSelectView into TimeseriesSelectView for Channels and Observatories.

--- test/spec/TimeseriesSelectViewTest.js
+++ test/spec/TimeseriesSelectViewTest.js
@@ -31,43 +31,10 @@ describe('TimeSeriesSelectView', function () {
   });
 
   describe('render()', function () {
-    var config,
-        view;
-
-    config = Model({
-      observatory: 'HON',
-      channel: 'E',
-      timemode: 'realtime'
-    });
-
-    view = TimeseriesSelectView({
-      config: config,
-      observatories: Collection([
-        {id: 'HON'}
-      ])
-    });
-
-    it('selects observatory', function () {
-      var selected = view.el.querySelector('.timeseries-observatory .selected');
-      expect(selected.getAttribute('data-id')).to.equal('HON');
-    });
-
-    it('selects channel', function () {
-      var selected = view.el.querySelector('.timeseries-channel .selected');
-      expect(selected.getAttribute('data-id')).to.equal('E');
-    });
-
     it('selects correct time radio input', function () {
-      expect(view.el.querySelector('#time-realtime').checked).to.equal(true);
-    });
+      var config,
+          view;
 
-  });
-
-  describe('onClick()', function () {
-    var config,
-        view;
-
-    beforeEach(function () {
       config = Model({
         observatory: 'HON',
         channel: 'E',
@@ -80,28 +47,8 @@ describe('TimeSeriesSelectView', function () {
           {id: 'HON'}
         ])
       });
-      document.querySelector('body').appendChild(view.el);
-    });
 
-    afterEach(function () {
-      Util.detach(view.el);
-      view.destroy();
-    });
-
-    it('observatory click updates config', function () {
-      var observatory = view.el.querySelector('.timeseries-observatory :first-child');
-      _fireClickEvent(observatory);
-      expect(config.get('observatory')).to.equal(
-          observatory.getAttribute('data-id'));
-      expect(config.get('channel')).to.equal(null);
-    });
-
-    it('channel click updates config', function () {
-      var channel = view.el.querySelector('.timeseries-channel :first-child');
-      _fireClickEvent(channel);
-      expect(config.get('channel')).to.equal(
-          channel.getAttribute('data-id'));
-      expect(config.get('observatory')).to.equal(null);
+      expect(view.el.querySelector('#time-realtime').checked).to.equal(true);
     });
   });
 
