934c80aa3eee87148413313c587a64d2db9e9dce
Jeremy Fee <jmfee@usgs.gov>
2017-02-10 14:56:14 -0700

    Add links to main plots and dst pages to navigation

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -1,6 +1,10 @@
 <?php
 
 // application injects its navigation into this element
+
 echo '<div id="geomag-config"></div>';
 
+echo navItem($MOUNT_PATH . '/index.php', 'Geomagnetism Plots');
+echo navItem($MOUNT_PATH . '/dst.php',
+		'<abbr title="Disturbance Storm Time">DST</abbr> Index');
 echo navItem('/ws/edge/', 'Web Service');
